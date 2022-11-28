37497d953bf3d09a0673739985135286ecf3adf1
SerhiyZv <skiev31@gmail.com>
2022-11-27 20:54:17 -0500

    Install dotenv and add basic structure for employees db and tables

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE employees_db;
+
+USE employee_db;
+
+-- Employee table 
+CREATE TABLE employee();
+
+-- Role table 
+CREATE TABLE role();
+
+-- Department table 
+CREATE TABLE department();
