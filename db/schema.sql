2d884dc5d32d15a7d5c7d92a8cac6a77059c4f8b
SerhiyZv <skiev31@gmail.com>
2022-11-27 22:54:59 -0500

    Update schema.sql with SQL to create DB and Tables in it

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,34 @@
-CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS employee_trackerDB;
 
-USE employee_db;
+CREATE DATABASE employee_trackerDB;
 
--- Employee table 
-CREATE TABLE employee();
-
--- Role table 
-CREATE TABLE role();
+USE employee_trackerDB;
 
 -- Department table 
-CREATE TABLE department();
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+-- Role table 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY(id)
+);
+
+-- Employee table 
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES role(id),
+    PRIMARY KEY(id)
+);
