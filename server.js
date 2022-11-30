0c13022d7260f84680f0f5498f817ac2c8fed869
SerhiyZv <skiev31@gmail.com>
2022-11-29 21:56:37 -0500

    Add the function with query to read from employee table

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ connection.connect((err) => {
         return;
     }
 
-    console.log(`connected as id  ${connection.threadId}`)
+    //console.log(`connected as id  ${connection.threadId}`)
+    //User questions
+    questions();
 });
 
 const questions = () => {
@@ -26,23 +28,62 @@ const questions = () => {
         message: 'What would ypu like to do?',
         choices: ['View All Employees', 'Add An Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
     }]).then((answer) => {
-        if(answer.questionList == 'View All Employees'){
+        // if(answer.questionList == 'View All Employees'){
             
-        }else if (answer.questionList == 'Add An Employee'){
+        // }else if (answer.questionList == 'Add An Employee'){
 
-        }else if (answer.questionList == 'Update Employee role'){
+        // }else if (answer.questionList == 'Update Employee role'){
 
-        }else if (answer.questionList == 'View All Roles'){
+        // }else if (answer.questionList == 'View All Roles'){
 
-        }else if (answer.questionList == 'Add Role'){
+        // }else if (answer.questionList == 'Add Role'){
 
-        }else if (answer.questionList == 'View All Departments'){
+        // }else if (answer.questionList == 'View All Departments'){
 
-        }else if (answer.questionList == 'Add Department'){
+        // }else if (answer.questionList == 'Add Department'){
 
+        // }
+        switch(answer.questionList){
+            case 'View All Employees':
+                viewAllEmployee();
+                break;
+            
+            case 'Add An Employee':
+                addAnEmployee();
+                break;
+
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                 break;
+
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+
+            case 'Add Role':
+                addRole();
+                break;
+
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+
+            case 'Add Department':
+                addDepartment();
+                break;
+
+            default:
+                break;
         }
     });
 }
+
+const viewAllEmployee = () => {
+    connection.query(`SELECT * FROM employee;`, (err, res) => {
+        console.table(res);
+        questions();
+    });
+};
 
 const init = async () => {
     console.log('Welcome To The Employee Content Management system!');
