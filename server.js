98d9c321162095ff8b023b50ae584ec528ec5536
SerhiyZv <skiev31@gmail.com>
2022-11-29 20:25:47 -0500

    Add questions for user

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
+const conTable = require("console.table");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
+const questions = () => {
+    inquirer.createPromptModule({
+        name: 'questionList',
+        type: 'list',
+        message: 'What would ypu like to do?',
+        choices: ['View All Employees', 'Add An Employee', 'Update Employee role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    }).then((answer) => {
+        if(answer.questionList == 'View All Employees'){
+            
+        }else if (answer.questionList == 'Add An Employee'){
+
+        }else if (answer.questionList == 'Update Employee role'){
+
+        }else if (answer.questionList == 'View All Roles'){
+
+        }else if (answer.questionList == 'Add Role'){
+
+        }else if (answer.questionList == 'View All Departments'){
+
+        }else if (answer.questionList == 'Add Department'){
+
+        }
+    });
+}
+
+const init = async () => {
+    console.log('Welcome To The Employee Content Management system!');
+    try {
+        questions();
+    } catch (err) {
+        console.log(err);
+        console.log('The was an error with user input')
+    }
+};
+
+init();
