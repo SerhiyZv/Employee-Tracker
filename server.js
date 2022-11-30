801721b3d2d90905eb9c733e226475b10cb1ed81
SerhiyZv <skiev31@gmail.com>
2022-11-29 21:18:54 -0500

    Fix prompt issue

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-const mysql2 = require("mysql2");
-const inquirer = require("inquirer");
-const conTable = require("console.table");
-const dotenv = require("dotenv");
+const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
-dotenv.config();
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employee_trackerdb'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return;
+    }
+
+    console.log(`connected as id  ${connection.threadId}`)
+});
 
 const questions = () => {
-    inquirer.createPromptModule({
+    inquirer.prompt([{
         name: 'questionList',
         type: 'list',
         message: 'What would ypu like to do?',
-        choices: ['View All Employees', 'Add An Employee', 'Update Employee role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
-    }).then((answer) => {
+        choices: ['View All Employees', 'Add An Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    }]).then((answer) => {
         if(answer.questionList == 'View All Employees'){
             
         }else if (answer.questionList == 'Add An Employee'){
